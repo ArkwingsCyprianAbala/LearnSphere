447923572928fb6bfee39b6e3428629af4ab8c1c
Arkwings Cyprian Abala <arkwingscyprianabala@gmail.com>
2026-05-26 17:11:29 +0300

    Fix settings.py: add missing os import and correct MEDIA_ROOT setup

--- app_users/models.py
+++ app_users/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
 
 # Create your models here.
+def path_and_rename(instance, filename):
+    upload_to = 'Images/'
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.user.username:
+        filename = 'User_Profile_Picture/{}.{}'.format(instance.user.username, ext)
+    return os.path.join(upload_to, filename)
 
-class user_profile(model.Model):
+class user_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     bio = models.CharField(max_length=150, blank=True)
 
-    profile_Pic = models.ImageField(upload_to=, blank=True)
+    profile_Pic = models.ImageField(upload_to=path_and_rename, verbose_name="Profile Picture", blank=True)
 
     teacher = 'teacher'
     student = 'student'
